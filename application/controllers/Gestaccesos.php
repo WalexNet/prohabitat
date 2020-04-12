cab6c7b9881ffc1a8ec5f5d03653985cc00059c1
WalexNet <walter@walex.net>
2020-04-12 19:35:03 +0200

    Simple actualizacion de Comentarios en código

---
+++ application/controllers/Gestaccesos.php
@@ -0,0 +1,36 @@
+<?php
+defined('BASEPATH') or exit('No direct script access allowed');
+
+
+/**
+ *
+ * Controller Gestaccesos
+ *
+ * This controller for ...
+ *
+ * @package   CodeIgniter
+ * @category  Controller CI
+ * @author    Daniel Walter Pérez Corvalán  <walter@walex.net>
+ * @link      www.walex.net
+ * @param     ...
+ * @return    ...
+ *
+ */
+
+class Gestaccesos extends CI_Controller
+{
+
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    public function index()
+    {
+        // 
+    }
+}
+
+
+/* End of file Gestaccesos.php */
+/* Location: ./application/controllers/Gestaccesos.php */
