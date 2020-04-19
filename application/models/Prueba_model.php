75966f79287798f2a2f8e041b8b2fa6e415e04f7
WalexNet <walter@walex.net>
2020-04-19 21:05:22 +0200

    Se me olvido un archivo Prueba_model.php

---
+++ application/models/Prueba_model.php
@@ -0,0 +1,42 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+/**
+ *
+ * Model Prueba_model
+ *
+ * This Model for ...
+ * 
+ * @package		CodeIgniter
+ * @category	Model
+ * @author    Daniel Walter Pérez Corvalán  <walter@walex.net>
+ * @link      www.walex.net
+ * @param     ...
+ * @return    ...
+ *
+ */
+
+class Prueba_model extends CI_Model {
+
+  // ------------------------------------------------------------------------
+
+  public function __construct()
+  {
+    parent::__construct();
+  }
+
+  // ------------------------------------------------------------------------
+
+
+  // ------------------------------------------------------------------------
+  public function index()
+  {
+    // 
+  }
+
+  // ------------------------------------------------------------------------
+
+}
+
+/* End of file Prueba_model.php */
+/* Location: ./application/models/Prueba_model.php */
